3ccd40cf417b8cb1bc74c21c82879554705aeeb5
vagrant <vagrant@precise>
2013-09-02 09:07:08 +0000

    ew file:   test-github/config/locales/en.yml

---
+++ test-github/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TestGithub::Application.config.secret_key_base = 'a417af5c9eff3f7d2856c3b800b20570d95df172039f58b577ed6ff7094d42b6f627899024d6161eda3ae990d1640d323739b04ea364ad3d447c3b11608c0156'
